6af84057848de2e977d81aa72f49eb92efa90339
shayloyuki <shayluo123@outlook.com>
2024-02-19 23:56:52 +0800

    用脚手架 create-react-app 创建项目

---
+++ 02-reactproject/src/App.js
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2024-02-19 23:26:46
+ * @LastEditors: shayloyuki shayluo123@outlook.com
+ * @LastEditTime: 2024-02-19 23:48:32
+ * @FilePath: \code-practice\reactproject\src\App.js
+ */
+function App() {
+  return (
+    <div>React 项目运行成功</div>
+  )
+}
+
+export default App
