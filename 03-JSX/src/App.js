f14b331fc1802da7aeb697da002ddbbef214951e
shayloyuki <shayluo123@outlook.com>
2024-02-20 08:07:38 +0800

    JSX 语法2

--- 03-JSX/src/App.js
+++ 03-JSX/src/App.js
@@ -1,43 +1,36 @@
 /*
  * @Date: 2024-02-19 23:26:46
  * @LastEditors: shayloyuki shayluo123@outlook.com
- * @LastEditTime: 2024-02-20 07:50:10
+ * @LastEditTime: 2024-02-20 08:04:21
  * @FilePath: \code-practice\03-JSX\src\App.js
  */
 
 /* 
- * 01 JSX 可以看作是 JS 语言的扩展，它既不是一个字符串也不是一个 HTML
- * 02 它具备了 JS 所有的功能，同时还可以被转为 HTML 在界面上展示（react react-dom）
+ * 01 JSX 本身就是一个表达式
  *
- * - 动态显示数据 {}
- * - 调用方法：自定义 + 内置
- * - 支持表达式，支持三元表达式
- * - 模板字符串
- * - 注释：要包裹
- * - 对象：JSON.striginfy() 展示
+ * 02 JSX 添加属性
+ *   字符串属性：直接用双引号包裹
+ *   动态属性：用花括号包裹
+ * 
+ * 03 JSX 添加子元素
+ * 
+ * 04 JSX 只能用一个父元素
+ * 
+ * 05 使用单标签时必须正确关闭
  *
  */
-
-const name = 'shayloyuki'
-
-function sayHello() {
-  return 'Hello'
-}
-
-const obj = {
-  name: 'shay',
-  age: 28
-}
+const name = <div>shayloyuki</div>
+const age = 28
 
 function App() {
   return (
     <div>
-      <p>{name}</p>
-      <p>name</p>
-      <p>{sayHello()}</p>
-      <p>{/* 注释内容 */}</p>
-      <p>{JSON.stringify(obj)}</p>
+      {name}
+      <p title="自定义标题">添加属性</p>
+      <p title={age}>添加动态属性</p>
+      <img />
     </div>
+
   )
 }
 
