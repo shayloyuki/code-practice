dafce58c9068c0340acb99432ecd4f80110537c3
shayloyuki <shayluo123@outlook.com>
2024-04-13 13:49:22 +0800

    学生信息管理系统：计算平均值等

--- 23-学生管理系统/src/components/StudentList.js
+++ 23-学生管理系统/src/components/StudentList.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2024-04-06 22:29:59
  * @LastEditors: shayloyuki shayluo123@outlook.com
- * @LastEditTime: 2024-04-13 12:56:26
+ * @LastEditTime: 2024-04-13 13:47:45
  * @FilePath: \23-学生管理系统\src\components\StudentList.js
  */
 import React, { Component } from "react";
@@ -15,6 +15,13 @@ class StudentList extends Component {
         removeStudent(number)
       }
     }
+    // 定义变量存放年龄之和
+    let totalAge = 0
+    // 定义变量存放平均年龄
+    let aveAge = 0
+    studentList.forEach(student => totalAge += student.age)
+    // 计算平均值
+    aveAge = Math.floor(totalAge / studentList.length)
     return (
       <div className={"col-md-6 col-md-offset-1"}>
         <table className="table table-striped table-hover">
@@ -44,18 +51,17 @@ class StudentList extends Component {
                 })}</td>
                 <td>{student.college}</td>
                 <td>
-                  {/* <a href="www.baidu.com" onClick={(ev) => rmStudent(`${student.number}`, ev)}>删除</a> */}
-                  <a href="www.baidu.com" onClick={(ev) => rmStudent(student.number, ev)}>删除</a>
-                  <a href="www.baidu.com">修改</a>
+                  {/* span" onClick={(ev) => rmStudent(`${student.number}`, ev)}>删除</span> */}
+                  <span onClick={(ev) => rmStudent(student.number, ev)}>删除</span>
                 </td>
               </tr>
               )
             })}
           </tbody>
         </table>
-        <p className="text-center">无学生信息</p>
-        <p>总共有 50 个学生</p>
-        <p>学生的平均年龄是 25</p>
+        {studentList.length ? null : <p className="text-center">无学生信息</p>}
+        <p>总共有 {studentList.length} 个学生</p>
+        <p>学生的平均年龄是 {aveAge}</p>
       </div>
     );
   }
