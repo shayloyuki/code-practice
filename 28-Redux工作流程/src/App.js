273add5dc02a8b5eb04bf7a669b265a0423b3dce
shayloyuki <shayluo123@outlook.com>
2024-04-17 07:38:30 +0800

    Redux工作流程

---
+++ 28-Redux工作流程/src/App.js
@@ -0,0 +1,27 @@
+/*
+ * @Date: 2024-04-05 13:23:34
+ * @LastEditors: shayloyuki shayluo123@outlook.com
+ * @LastEditTime: 2024-04-17 07:37:45
+ * @FilePath: \28-Redux工作流程\src\App.js
+ */
+/* 
+ * 01 为什么需要 Redux
+ * 02 Redux 是什么
+ * 
+ *   Redux 是一个数据管理框架，它提供了一个叫 store 的统一数据存储的仓库
+ *   store 就像是一个数据管理的中间人，让组件之间无须再直接进行数据传递
+ */
+
+import React, { Component } from "react";
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>Redux工作流程</div>
+    );
+  }
+
+}
+
+export default App;
---
+++ 28-Redux工作流程/src/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Date: 2024-04-05 13:23:34
+ * @LastEditors: shayloyuki shayluo123@outlook.com
+ * @LastEditTime: 2024-04-06 22:14:07
+ * @FilePath: \23-学生管理系统\src\index.js
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
