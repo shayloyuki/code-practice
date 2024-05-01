8246f4af10bbb9fa0a74c137e6835c802d866ae1
shayloyuki <shayluo123@outlook.com>
2024-05-01 23:47:11 +0800

    路由跳转及重定向

--- React/3.React Hooks/src/components/detail.js
+++ React/3.React Hooks/src/components/detail.js
@@ -1,15 +1,19 @@
 /*
  * @Date: 2024-05-01 15:28:32
  * @LastEditors: shayloyuki shayluo123@outlook.com
- * @LastEditTime: 2024-05-01 16:54:03
+ * @LastEditTime: 2024-05-01 23:42:06
  * @FilePath: \3.React Hooks\src\components\detail.js
  */
 import React, {Component} from "react";
+import {createHashHistory} from 'history'
 
 class Detail extends Component {
   render() {
     return (
-      <div>{this.props.match.params.id}</div>
+      <div>
+        <p>{this.props.match.params.id}</p>
+        <button onClick={() => createHashHistory().push('/home')}>点击跳转首页</button>
+      </div>
     )
   }
 }
--- React/3.React Hooks/src/components/home.js
+++ React/3.React Hooks/src/components/home.js
@@ -1,14 +1,21 @@
 /*
  * @Date: 2024-05-01 15:28:32
  * @LastEditors: shayloyuki shayluo123@outlook.com
- * @LastEditTime: 2024-05-01 22:35:11
+ * @LastEditTime: 2024-05-01 23:44:35
  * @FilePath: \3.React Hooks\src\components\home.js
  */
 import React, {Component} from "react";
 import qs from 'qs'
+import { Redirect } from "react-router-dom";
 
 class Home extends Component {
   render() {
+    let isLogin = false
+
+    if (!isLogin) {
+      return <Redirect to="/list"/>
+    }
+
     return (
       <div>Home 首页内容 {qs.parse(this.props.location.search, {ignoreQueryPrefix: true}).title}</div>
     )
